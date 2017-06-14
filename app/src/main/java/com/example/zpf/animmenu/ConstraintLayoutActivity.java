0496ed6f5f90070a0a35fae17831337f0042463e
nsacer <1250855973@qq.com>
2017-06-14 21:22:00 +0800

    ConstraintLayoutActivity

--- app/src/main/java/com/example/zpf/animmenu/ConstraintLayoutActivity.java
+++ app/src/main/java/com/example/zpf/animmenu/ConstraintLayoutActivity.java
@@ -1,7 +1,6 @@
 package com.example.zpf.animmenu;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
