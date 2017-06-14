90d97434e8a079938fbf4aa7f51592ce38ffcfc3
nsacer <1250855973@qq.com>
2017-06-14 23:10:08 +0800

    ConstraintLayoutActivity

--- app/src/main/java/com/example/zpf/animmenu/ConstraintLayoutActivity.java
+++ app/src/main/java/com/example/zpf/animmenu/ConstraintLayoutActivity.java
@@ -1,15 +1,24 @@
 package com.example.zpf.animmenu;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.Event;
+import org.xutils.view.annotation.ViewInject;
 
 @ContentView(R.layout.activity_constraint_layout)
 public class ConstraintLayoutActivity extends BaseActivity {
+
+    @ViewInject(R.id.appbar_layout)
+    private AppBarLayout appBarLayout;
+
+    @ViewInject(R.id.constraint_layout_coupon)
+    private ConstraintLayout constraintLayoutCoupon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +40,28 @@ public class ConstraintLayoutActivity extends BaseActivity {
 
         Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show();
+    }
+
+    /**
+     * 弹出RecyclerView
+     */
+    @Event(R.id.btn_one)
+    private void clickBtnOne(View view) {
+
+        showToast(view.getId());
+
+        appBarLayout.setVisibility(View.GONE);
+        constraintLayoutCoupon.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * 收起RecyclerView
+     */
+    @Event(R.id.btn_coupon)
+    private void clickBtnCoupon(View view) {
+
+        appBarLayout.setVisibility(View.VISIBLE);
+        constraintLayoutCoupon.setVisibility(View.GONE);
     }
 
 }
