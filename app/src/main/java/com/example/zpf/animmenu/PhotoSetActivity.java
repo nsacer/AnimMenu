b6fce284af486cf38276e33cee7a59fc97ca0a24
Pary <1250855973@qq.com>
2017-07-20 18:54:00 +0800

    图片选择更改。

--- app/src/main/java/com/example/zpf/animmenu/PhotoSetActivity.java
+++ app/src/main/java/com/example/zpf/animmenu/PhotoSetActivity.java
@@ -9,7 +9,6 @@ import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -29,23 +28,24 @@ import android.widget.PopupWindow;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+
 import me.nereo.multi_image_selector.MultiImageSelector;
 import me.nereo.multi_image_selector.utils.FileUtils;
 import utils.AnimMenuSetting;
 
 public class PhotoSetActivity extends AppCompatActivity {
 
+    protected static final int REQUEST_STORAGE_READ_ACCESS_PERMISSION = 101;
+    protected static final int REQUEST_STORAGE_WRITE_ACCESS_PERMISSION = 102;
     private static final int REQUEST_IMAGE = 0X000;
     private static final int REQUEST_CAMERA = 0X001;
     private static final int REQUEST_IMAGE_CROP = 0X002;
-    protected static final int REQUEST_STORAGE_READ_ACCESS_PERMISSION = 101;
-    protected static final int REQUEST_STORAGE_WRITE_ACCESS_PERMISSION = 102;
-
     private RadioGroup rgSelectMode, rgShowMode;
     private EditText etSelectNum;
     private ImageView ivHeader;
@@ -53,14 +53,35 @@ public class PhotoSetActivity extends AppCompatActivity {
     private TextView tvPictureInfo;
     private ArrayList<String> imgList = new ArrayList<>();
 
-    /** 拍照存储的图片文件 */
+    /**
+     * 拍照存储的图片文件
+     */
     private File mTmpFile;
+
+    /**
+     * 根据图片地址获取Bitmap
+     *
+     * @param url:本地图片的地址
+     * @return Bitmap:返回读取到的图片
+     */
+    public static Bitmap getLocalBitmap(String url) {
+
+        if (TextUtils.isEmpty(url)) {
+            return null;
+        }
+        try {
+            FileInputStream fis = new FileInputStream(url);
+            return BitmapFactory.decodeStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_set);
-
 
         initView();
 
@@ -95,7 +116,6 @@ public class PhotoSetActivity extends AppCompatActivity {
         popupWindow.setOutsideTouchable(true);
         popupWindow.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
     }
-
 
     /**
      * 弹出选取头像方式：拍照、相册
@@ -149,9 +169,9 @@ public class PhotoSetActivity extends AppCompatActivity {
 
         if (resultCode != RESULT_OK) {
             //拍完照片但是不OK时候删除缓存照片
-            while (mTmpFile != null && mTmpFile.exists()){
+            while (mTmpFile != null && mTmpFile.exists()) {
                 boolean success = mTmpFile.delete();
-                if(success){
+                if (success) {
                     mTmpFile = null;
                 }
             }
@@ -166,7 +186,7 @@ public class PhotoSetActivity extends AppCompatActivity {
 
                 StringBuilder sb = new StringBuilder();
 
-                for (String sImg : imgList){
+                for (String sImg : imgList) {
 
                     sb.append(sImg);
                     sb.append("\n");
@@ -203,12 +223,12 @@ public class PhotoSetActivity extends AppCompatActivity {
      * Open camera打开相机拍照
      */
     private void showCameraAction() {
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-            != PackageManager.PERMISSION_GRANTED){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
             requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,
                     getString(me.nereo.multi_image_selector.R.string.mis_permission_rationale_write_storage),
                     REQUEST_STORAGE_WRITE_ACCESS_PERMISSION);
-        }else {
+        } else {
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (intent.resolveActivity(this.getPackageManager()) != null) {
                 try {
@@ -228,15 +248,15 @@ public class PhotoSetActivity extends AppCompatActivity {
         }
     }
 
-    /** 从相册选取照片 */
+    /**
+     * 从相册选取照片
+     */
     private void pickImage() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN // Permission was added in API Level 16
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
-                != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE,
                     getString(R.string.mis_permission_rationale),
                     REQUEST_STORAGE_READ_ACCESS_PERMISSION);
-        }else {
+        } else {
             boolean showCamera = rgShowMode.getCheckedRadioButtonId() == R.id.rb_show_camera;
             int maxNum = 9;
 
@@ -261,9 +281,11 @@ public class PhotoSetActivity extends AppCompatActivity {
         }
     }
 
-    /** 没有权限弹窗请求权限 */
-    private void requestPermission(final String permission, String rationale, final int requestCode){
-        if(ActivityCompat.shouldShowRequestPermissionRationale(this, permission)){
+    /**
+     * 没有权限弹窗请求权限
+     */
+    private void requestPermission(final String permission, String rationale, final int requestCode) {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
             new AlertDialog.Builder(this)
                     .setTitle(R.string.mis_permission_dialog_title)
                     .setMessage(rationale)
@@ -275,23 +297,25 @@ public class PhotoSetActivity extends AppCompatActivity {
                     })
                     .setNegativeButton(R.string.mis_permission_dialog_cancel, null)
                     .create().show();
-        }else{
+        } else {
             ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode);
         }
     }
 
-    /** 请求权限结果处理 */
+    /**
+     * 请求权限结果处理
+     */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         //读取相册的权限
-        if(requestCode == REQUEST_STORAGE_READ_ACCESS_PERMISSION){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == REQUEST_STORAGE_READ_ACCESS_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 pickImage();
             }
         }
         //拍照写入照片的权限
-        else if(requestCode == REQUEST_STORAGE_WRITE_ACCESS_PERMISSION){
-            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        else if (requestCode == REQUEST_STORAGE_WRITE_ACCESS_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 showCameraAction();
             }
         } else {
@@ -300,39 +324,23 @@ public class PhotoSetActivity extends AppCompatActivity {
     }
 
     /**
-     * 根据图片地址获取Bitmap
-     *
-     * @param url:本地图片的地址
-     * @return Bitmap:返回读取到的图片
+     * 跳转到图片裁剪界面
      */
-    public static Bitmap getLocalBitmap(String url) {
+    private void goToCropActivity(String sSourcePath) {
 
-        if(TextUtils.isEmpty(url)){
-            return null;
-        }
-        try {
-            FileInputStream fis = new FileInputStream(url);
-            return BitmapFactory.decodeStream(fis);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /** 跳转到图片裁剪界面 */
-    private void goToCropActivity(String sSourcePath){
-
-        if(TextUtils.isEmpty(sSourcePath))
+        if (TextUtils.isEmpty(sSourcePath))
             return;
-        Intent intent2=new Intent(this, ClipActivity.class);
+        Intent intent2 = new Intent(this, ClipActivity.class);
         intent2.putExtra(AnimMenuSetting.IMAGE_SOURCE_PATH, sSourcePath);
         startActivityForResult(intent2, REQUEST_IMAGE_CROP);
     }
 
-    /** 返回按钮处理 */
+    /**
+     * 返回按钮处理
+     */
     @Override
     public void onBackPressed() {
-        if(popupWindow.isShowing()) {
+        if (popupWindow.isShowing()) {
 
             popupWindow.dismiss();
             return;
